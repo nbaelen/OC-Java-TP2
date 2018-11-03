344f1f93ce143b879a9312e908a8700ed9d18037
nbaelen <n.baelen@gmail.com>
2018-11-03 23:39:48 +0100

    Structure de la classe Garage

--- src/Garage.java
+++ src/Garage.java
@@ -1,2 +1,16 @@
+import java.util.List;
+
 public class Garage {
+
+    //Attributs
+    private List<Vehicule> voitures;
+
+    public String toString() {
+
+        return "";
+    }
+
+    public void add(Vehicule voit) {
+        this.voitures.add(voit);
+    }
 }
