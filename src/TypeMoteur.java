483f156b218e2b61908de3847662f07adc821e45
nbaelen <n.baelen@gmail.com>
2018-11-03 23:30:38 +0100

    Structure de l'énumérateur TypeMoteur

--- src/TypeMoteur.java
+++ src/TypeMoteur.java
@@ -1,2 +1,13 @@
 public enum TypeMoteur {
+
+    DIESEL("DIESEL"),
+    ESSENCE("ESSENCE"),
+    HYBRIDE("HYBRIDE"),
+    ELECTRIQUE("ELECTRIQUE");
+
+    private String typeMoteur;
+
+    TypeMoteur(String typeMoteur) {
+        this.typeMoteur = typeMoteur;
+    }
 }
