5cf4bea41319a3dc025d6b7cccfe24c35d1827a0
nbaelen <n.baelen@gmail.com>
2018-11-03 23:41:15 +0100

    Structure de l'énumération Marque et modification TypeMoteur

--- src/Marque.java
+++ src/Marque.java
@@ -1,2 +1,5 @@
 public enum Marque {
+    RENO,
+    PIGEOT,
+    TROEN;
 }
--- src/TypeMoteur.java
+++ src/TypeMoteur.java
@@ -1,13 +1,6 @@
 public enum TypeMoteur {
-
-    DIESEL("DIESEL"),
-    ESSENCE("ESSENCE"),
-    HYBRIDE("HYBRIDE"),
-    ELECTRIQUE("ELECTRIQUE");
-
-    private String typeMoteur;
-
-    TypeMoteur(String typeMoteur) {
-        this.typeMoteur = typeMoteur;
-    }
+    DIESEL,
+    ESSENCE,
+    HYBRIDE,
+    ELECTRIQUE;
 }
