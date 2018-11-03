ec863dd1b9ee56d261473a620c563db8771e7a5b
nbaelen <n.baelen@gmail.com>
2018-11-03 23:21:59 +0100

    Structure de la classe Moteur

--- src/Moteur.java
+++ src/Moteur.java
@@ -1,2 +1,19 @@
 public class Moteur {
+
+    //Attributs
+    protected TypeMoteur type;
+    protected String cylindre;
+    protected Double prix;
+
+    public void Moteur(String cylindre, Double prix) {
+
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public Double getPrix() {
+        return this.prix;
+    }
 }
