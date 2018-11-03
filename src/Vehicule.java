8a94cabf82058a62844ead9f59661a1f682c7875
nbaelen <n.baelen@gmail.com>
2018-11-03 23:16:23 +0100

    Structure de la classe Véhicule

--- src/Vehicule.java
+++ src/Vehicule.java
@@ -1,2 +1,39 @@
+import java.util.List;
+
 public class Vehicule {
+
+    //Attributs
+    protected Double prix;
+    protected String nom;
+    protected List<Option> options;
+    protected Marque nomMarque;
+    protected Moteur moteur;
+
+    public String toString() {
+        for (Option opt: getOptions()) {
+            
+        }
+        
+        return "";
+    }
+
+    public void addOption(Option opt) {
+        this.options.add(opt);
+    }
+
+    public Marque getMarque() {
+        return this.nomMarque;
+    }
+
+    public List<Option> getOptions() {
+        return this.getOptions();
+    }
+
+    public Double getPrix() {
+        return this.prix;
+    }
+
+    public Moteur getMoteur() {
+        return this.moteur;
+    }
 }
